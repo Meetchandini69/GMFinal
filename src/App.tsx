f6f1ea67eeb18d3317e1d5677bb0314642fe6ed1
mageswari <mages.sbca@gmail.com>
2026-07-22 18:24:34 +0530

    coimbatore page added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Admin from './pages/Admin';
+import Coimbatore from './pages/Coimbatore';
 import NotFound from './pages/not-found';
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
       <Route path="/login" component={Login} />
       <Route path="/dashboard" component={Dashboard} />
       <Route path="/admin" component={Admin} />
+      <Route path="/coimbatore" component={Coimbatore} />
       <Route component={NotFound} />
     </Switch>
   );
